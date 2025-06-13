8a1afae4daab3f54a30799765065c68853d85402
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-06-13 12:53:18 -0300

    fixCors03

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+# import os
+#
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456@localhost:5432/alunosdb")
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123456@localhost:5432/alunosdb")
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://usuario:senha@localhost:5432/alunosdb")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
